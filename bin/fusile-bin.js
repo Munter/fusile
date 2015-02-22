3230dbf404277a84d390c542ae1dbd3ac90dcfef
Peter Müller <munter@fumle.dk>
2015-02-22 01:30:49 +0100

    Added binary

---
+++ bin/fusile-bin.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+'use strict';
+
+var argv = require('minimist')(process.argv.slice(2));
+var path = require('path');
+var fusile = require('../lib/index');
+var mkdirp = require('mkdirp');
+
+if (argv._.length < 2 || argv.h) {
+  console.error('Usage: $0 <sourceDir> <targetDir>');
+  process.exit(1);
+}
+
+var sourceDir = path.resolve(process.cwd(), argv._[0]);
+var mountPoint = path.resolve(process.cwd(), argv._[1]);
+
+mkdirp(mountPoint, function () {
+  fusile(sourceDir, mountPoint);
+
+  // Start reading from stdin
+  process.stdin.resume();
+
+  console.log('File system started at ' + mountPoint);
+  console.log('To stop it, press Ctrl+C');
+
+  process.on('SIGINT', function () {
+    require('../lib/unmount')(mountPoint, function () {
+      console.error('\nUnmounted: ' + mountPoint);
+
+      process.exit(0);
+    });
+  });
+});
