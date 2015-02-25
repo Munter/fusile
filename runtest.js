863bfa428f2061857a7f83acef1c307d249a8a52
Peter Müller <munter@fumle.dk>
2015-02-26 00:27:43 +0100

    Added verbose manual test of file watching

--- runtest.js
+++ runtest.js
@@ -8,7 +8,38 @@ var testTarget = '__TEST_MOUNT_POINT';
 
 mkdirp(testTarget);
 
-fusile('test/fixtures/source', testTarget);
+fusile('test/fixtures/source', testTarget, {
+  watches: ['**/*.*'],
+  verbose: true
+});
+
+///////
+
+var fs = require('fs');
+var path = require('path');
+
+var origFile = path.join(process.cwd(), 'test/fixtures/source', 'unchanged.txt');
+var fuseFile = path.join(process.cwd(), testTarget, 'unchanged.txt');
+var shortFuseFile = path.join(testTarget, 'unchanged.txt');
+
+setTimeout(function () {
+  fs.watch(shortFuseFile, function (event, pathname) {
+    console.log('watcher', event, pathname);
+  });
+
+  setInterval(function () {
+    console.log('Touching', origFile);
+    fs.utimes(origFile, new Date(), new Date());
+  }, 1000);
+
+  setTimeout(function () {
+    fs.readFile(fuseFile, 'utf-8', function (err, data) {
+      console.log('readFile', err, data);
+    });
+  }, 2000);
+}, 100);
+
+///////
 
 // Start reading from stdin
 process.stdin.resume();
