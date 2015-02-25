73968d220ff671af46f63ff2320ec1f3f514065b
Peter Müller <munter@fumle.dk>
2015-02-25 22:56:15 +0100

    Improve watchers command line input and set up hook for matching watcher patterns

--- bin/fusile-bin.js
+++ bin/fusile-bin.js
@@ -7,16 +7,18 @@ var fusile = require('../lib/index');
 var mkdirp = require('mkdirp');
 
 if (argv._.length < 2 || argv.h) {
-  console.error('Usage: $0 <sourceDir> <targetDir>');
+  console.error('Usage: $0 <sourceDir> <targetDir> [-v] [-w path/to/watchTarget.scss]');
   process.exit(1);
 }
 
 var sourceDir = path.resolve(process.cwd(), argv._[0]);
 var mountPoint = path.resolve(process.cwd(), argv._[1]);
 
+var watches = [].concat(argv.watch || [], argv.w || []);
+
 mkdirp(mountPoint, function () {
   fusile(sourceDir, mountPoint, {
-    watches: argv.watch,
+    watches: watches,
     verbose: argv.v
   });
 
