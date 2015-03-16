281efeb1be6998ce240f8bcb840da76cde21cd67
Peter Müller <munter@fumle.dk>
2015-03-16 21:38:14 +0100

    Stop file updating in runtest

--- runtest.js
+++ runtest.js
@@ -15,29 +15,29 @@ fusile('fixtures/source', testTarget, {
 
 ///////
 
-var fs = require('fs');
-var path = require('path');
+// var fs = require('fs');
+// var path = require('path');
 
-var origFile = path.join(process.cwd(), 'test/fixtures/source', 'unchanged.txt');
-var fuseFile = path.join(process.cwd(), testTarget, 'unchanged.txt');
-var shortFuseFile = path.join(testTarget, 'unchanged.txt');
+// var origFile = path.join(process.cwd(), 'test/fixtures/source', 'unchanged.txt');
+// var fuseFile = path.join(process.cwd(), testTarget, 'unchanged.txt');
+// var shortFuseFile = path.join(testTarget, 'unchanged.txt');
 
-setTimeout(function () {
-  fs.watch(shortFuseFile, function (event, pathname) {
-    console.log('watcher', event, pathname);
-  });
+// setTimeout(function () {
+//   fs.watch(shortFuseFile, function (event, pathname) {
+//     console.log('watcher', event, pathname);
+//   });
 
-  setInterval(function () {
-    console.log('Touching', origFile);
-    fs.utimes(origFile, new Date(), new Date());
-  }, 1000);
+//   setInterval(function () {
+//     console.log('Touching', origFile);
+//     fs.utimes(origFile, new Date(), new Date());
+//   }, 1000);
 
-  setTimeout(function () {
-    fs.readFile(fuseFile, 'utf-8', function (err, data) {
-      console.log('readFile', err, data);
-    });
-  }, 2000);
-}, 100);
+//   setTimeout(function () {
+//     fs.readFile(fuseFile, 'utf-8', function (err, data) {
+//       console.log('readFile', err, data);
+//     });
+//   }, 2000);
+// }, 100);
 
 ///////
 
