4bcbf27a3c559b61f1f9e9617103e1f103154da4
Peter Müller <munter@fumle.dk>
2015-06-01 19:48:27 +0200

    Upgrade unexpected to 7.5.1 and adjust tests for it

--- test/readCompiled-test.js
+++ test/readCompiled-test.js
@@ -12,32 +12,22 @@ function getPath(path) {
 
 describe('readCompiled', function () {
   it('should read a file directly if there is no adapter', function () {
-    return readCompiled(getPath('unchanged.txt')).then(function (result) {
-      return expect(result, 'to be', 'I am the same\n');
-    });
+    return expect(readCompiled(getPath('unchanged.txt')), 'to be resolved with', 'I am the same\n');
   });
 
   it('should throw when reading a file that does not exist', function () {
-    return readCompiled(getPath('does-not-exist.txt')).catch(function (err) {
-      return expect(err, 'to have message', 'ENOENT, open \'/home/munter/git/fusile/fixtures/source/does-not-exist.txt\'');
-    });
+    return expect(readCompiled(getPath('does-not-exist.txt')), 'to be rejected with', 'ENOENT, open \'/home/munter/git/fusile/fixtures/source/does-not-exist.txt\'');
   });
 
   it('should compile a file if there is an adapter', function () {
-    return readCompiled(getPath('babel/simplest.jsx')).then(function (result) {
-      return expect(result, 'to be', '"use strict";\n\nvar foo = "bar";\n//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi9ob21lL211bnRlci9naXQvZnVzaWxlL2ZpeHR1cmVzL3NvdXJjZS9iYWJlbC9zaW1wbGVzdC5qc3giXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSxJQUFNLEdBQUcsR0FBRyxLQUFLLENBQUMiLCJmaWxlIjoiL2hvbWUvbXVudGVyL2dpdC9mdXNpbGUvZml4dHVyZXMvc291cmNlL2JhYmVsL3NpbXBsZXN0LmpzeCJ9\n');
-    });
+    return expect(readCompiled(getPath('babel/simplest.jsx')), 'to be resolved with', '"use strict";\n\nvar foo = "bar";\n//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi9ob21lL211bnRlci9naXQvZnVzaWxlL2ZpeHR1cmVzL3NvdXJjZS9iYWJlbC9zaW1wbGVzdC5qc3giXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSxJQUFNLEdBQUcsR0FBRyxLQUFLLENBQUMiLCJmaWxlIjoiL2hvbWUvbXVudGVyL2dpdC9mdXNpbGUvZml4dHVyZXMvc291cmNlL2JhYmVsL3NpbXBsZXN0LmpzeCJ9\n');
   });
 
   it('should autoprefix uncompiled CSS output', function () {
-    return readCompiled(getPath('basic.css')).then(function (result) {
-      return expect(result, 'to be', 'body {\n  -webkit-transform: rotate(-1deg);\n          transform: rotate(-1deg);\n}\n');
-    });
+    return expect(readCompiled(getPath('basic.css')), 'to be resolved with', 'body {\n  -webkit-transform: rotate(-1deg);\n          transform: rotate(-1deg);\n}\n');
   });
 
   it('should autoprefix compiled CSS output', function () {
-    return readCompiled(getPath('scss/autoprefix.scss')).then(function (result) {
-      return expect(result, 'to be', 'body {\n  -webkit-transform: rotate(-1deg);\n          transform: rotate(-1deg); }\n\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi9ob21lL211bnRlci9naXQvZnVzaWxlL2ZpeHR1cmVzL3NvdXJjZS9zY3NzL2F1dG9wcmVmaXguc2NzcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFFQTtFQUNFLGtDQUFXO1VBQVgsMEJBQVcsRUFBQSIsImZpbGUiOiJ0by5jc3MifQ== */');
-    });
+    return expect(readCompiled(getPath('scss/autoprefix.scss')), 'to be resolved with', 'body {\n  -webkit-transform: rotate(-1deg);\n          transform: rotate(-1deg); }\n\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi9ob21lL211bnRlci9naXQvZnVzaWxlL2ZpeHR1cmVzL3NvdXJjZS9zY3NzL2F1dG9wcmVmaXguc2NzcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFFQTtFQUNFLGtDQUFXO1VBQVgsMEJBQVcsRUFBQSIsImZpbGUiOiJ0by5jc3MifQ== */');
   });
 });
